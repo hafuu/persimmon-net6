1f0b39e3e4b19dbe63f3345c48bcb998bea5bf1b
hafuu <hafuu46@gmail.com>
2022-02-20 19:53:36 +0900

    Persimmon5.0でテストを実行

---
+++ TestLibrary/Program.fs
@@ -0,0 +1,9 @@
+﻿module Program
+
+open Persimmon.Console
+open System.Reflection
+
+[<EntryPoint>]
+let main argv =
+  let args = Args.parse Args.empty (argv |> Array.toList)
+  Runner.runTestsInAssembly (args) [ Assembly.GetExecutingAssembly() ]
